804df20be13de7e24c8b8e1e58555ef6bf6157d9
Amit Verma <amit.verma@consultbae.com>
2022-10-28 12:23:40 +0530

    Fixed logger service format issues

--- apps/example-service/src/app/config/config.controller.ts
+++ apps/example-service/src/app/config/config.controller.ts
@@ -6,6 +6,7 @@ import { IDBConfiguration } from '@spotlyt-backend/data/interfaces';
 @Controller('config')
 export class ConfigController {
 
+    private readonly logger = new Logger(ConfigController.name);
     constructor(
         private readonly configService: ConfigService
     ){}
@@ -15,12 +16,12 @@ export class ConfigController {
         // get config from env
         const env: string = this.configService.get<string>(NODE_ENV);
         if (env === "development"){
-            Logger.log(`Current ENV is ${env}`);
+            this.logger.log(`Current ENV is ${env}`);
         }
         const port: string = this.configService.get<string>("PORT");
         // get config from file
         const dbConfig: IDBConfiguration = this.configService.get<IDBConfiguration>('database'); 
-        Logger.log(`Database host ${dbConfig.host} and port ${dbConfig.port}` );
+        this.logger.log(`Database host ${dbConfig.host} and port ${dbConfig.port}` );
         return `Service is running at ${port}`;
     }
 
--- libs/common/src/lib/logging/logging.model.ts
+++ libs/common/src/lib/logging/logging.model.ts
@@ -1,38 +1,34 @@
 import * as winston from 'winston';
 import * as winstonDailyRotateFile from 'winston-daily-rotate-file';
-import { WinstonModule } from 'nest-winston';
+import { WinstonModule, utilities as nestWinstonModuleUtilities } from 'nest-winston';
 
-const transports = {
-  console: new winston.transports.Console({
-    level: 'silly',
-    format: winston.format.combine(
-      winston.format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss',
-      }),
-      winston.format.colorize({ all: true }),
-      winston.format.printf((info) => {
-        return `[Nest] - ${info.service}  -- ${info.timestamp} ${info.level} [${
-          info.context ? info.context : info.stack
-        }] ${info.message}`;
-      }),
-      winston.format.align(),
-    ),
-  }),
-  combinedFile: new winstonDailyRotateFile({
-    dirname: 'logs',
-    filename: 'combined',
-    extension: '.log',
-    level: 'info',
-  }),
-  errorFile: new winstonDailyRotateFile({
-    dirname: 'logs',
-    filename: 'error',
-    extension: '.log',
-    level: 'error',
-  }),
-};
 
 export const initWinston = (apiTitle: string) => {
+  const transports = {
+    console: new winston.transports.Console({
+      level: 'silly',
+      format: winston.format.combine(
+        winston.format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        winston.format.ms(),
+        nestWinstonModuleUtilities.format.nestLike(apiTitle),
+      ),
+    }),
+    combinedFile: new winstonDailyRotateFile({
+      dirname: 'logs',
+      filename: 'combined',
+      extension: '.log',
+      level: 'info',
+    }),
+    errorFile: new winstonDailyRotateFile({
+      dirname: 'logs',
+      filename: 'error',
+      extension: '.log',
+      level: 'error',
+    }),
+  };
+  
   return WinstonModule.createLogger({
     format: winston.format.combine(
       winston.format.timestamp({
