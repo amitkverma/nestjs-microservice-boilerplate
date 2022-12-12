de0b504d2bed44393dd6ddfa4688f031466bf40f
Sourav Ahmed <souravsunju@gmail.com>
2022-12-12 12:58:04 +0530

    frontend specific err

--- libs/common/src/lib/filters/exception.filter.ts
+++ libs/common/src/lib/filters/exception.filter.ts
@@ -40,6 +40,6 @@ export class AllExceptionsFilter extends BaseExceptionFilter {
             errorObj.message = (exception as PrismaClientKnownRequestError).message
             errorObj.meta = (exception as PrismaClientKnownRequestError).meta
         }
-        return response.status(HttpStatus.NOT_FOUND).json({ statusCode: (exception as IHTTPError).status, error: (exception as HttpException).name, message: (exception as HttpException).message, meta: (exception as HttpException).cause });
+        return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ statusCode: (exception as IHTTPError).status, error: (exception as HttpException).name, message: (exception as HttpException).message, meta: (exception as HttpException).cause });
     }
 }
